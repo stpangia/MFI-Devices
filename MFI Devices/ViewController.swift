c1adf224ea9da9520abe6ad94abbc986d7f0329a
stpangia <sean@redcent.net>
2019-10-06 08:46:17 -0400

    Initial Commit

---
+++ MFI Devices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MFI Devices
+//
+//  Created by Sean Pangia on 10/6/19.
+//  Copyright © 2019 Sean Pangia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
